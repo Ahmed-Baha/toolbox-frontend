f011224240c6f11105375f79e3545c7f6ab19829
ahmed bahaa <ahmedbahaa948@gmail.com>
2025-11-19 15:19:39 +0200

    edit api2

--- src/api/client.js
+++ src/api/client.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // In production, API calls will be made to the same domain
 // In development, use the full backend URL with /api appended
-const apiUrl =`${import.meta.env.VITE_BACKEND_URL || 'http://localhost:3003'}/api` // Development: full URL + /api
+const apiUrl =`${import.meta.env.VITE_BACKEND_URL}/api` // Development: full URL + /api
 
 export default axios.create({
   baseURL: apiUrl,
